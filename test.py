3ebc86d89c3c231cc3d9b8491a14ea366c7e53b0
KamdenB <kambol2003@aol.com>
2020-11-30 16:06:44 -0500

    Test updated

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-import platform
-print(platform.architecture()[0])
+import RPi.GPIO as GPIO
+import time
+channel = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.OUT)
+
+def on(pin):
+    GPIO.output(pin, GPIO.HIGH)
+
+def off(pin):
+    GPIO.outpout(pin, GPIO.LOW)
+
+if __name__ == '__main__':
+    try:
+        on(channel)
+        time.sleep(1)
+        off(channel)
+        time.sleep(1)
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        pass
